51827d9408a844c3fc569a1ffccede085317a997
Justin Yoo <justin.yoojh@gmail.com>
2018-08-05 22:35:18 +1000

    Add ARM template tests

---
+++ LogicApp.Tests.ps1
@@ -0,0 +1,51 @@
+﻿#
+# This tests whether the ARM template for Logic App has been properly deployed or not.
+#
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory=$true)] $SrcDirectory,
+    [string] [Parameter(Mandatory=$true)] $Username,
+    [string] [Parameter(Mandatory=$true)] $Password,
+    [string] [Parameter(Mandatory=$true)] $TenantId
+)
+
+Describe "Logic App Deployment Tests" {
+    # Init
+    BeforeAll {
+        az login --service-principal -u $Username -p $Password -t $TenantId
+    }
+
+    # Teardown
+    AfterAll {
+    }
+
+    # Tests whether the cmdlet returns value or not.
+    Context "When Logic App deployed with parameters" {
+        $output = az group deployment validate `
+            -g $ResourceGroupName `
+            --template-file $SrcDirectory\azuredeploy.json `
+            --parameters `@$SrcDirectory\azuredeploy.parameters.json `
+            | ConvertFrom-Json
+        
+        $result = $output.properties
+
+        It "Should be deployed successfully" {
+            $result.provisioningState | Should -Be "Succeeded"
+        }
+
+        It "Should have state of" {
+            $expected = "Enabled"
+            $resource = $result.validatedResources[0]
+
+            $resource.properties.state | Should -Be $expected
+        }
+
+        It "Should have name of" {
+            $expected = "my-logic-app"
+            $resource = $result.validatedResources[0]
+
+            $resource.name | Should -Be $expected
+        }
+    }
+}
---
+++ Run-Pester.ps1
@@ -0,0 +1,23 @@
+﻿#
+# This invokes pester test run.
+#
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory=$true)] $TestFilePath,
+    [string] [Parameter(Mandatory=$true)] $SrcDirectory,
+    [string] [Parameter(Mandatory=$true)] $OutputDirectory,
+    [string] [Parameter(Mandatory=$true)] $BuildNumber,
+    [string] [Parameter(Mandatory=$true)] $Username,
+    [string] [Parameter(Mandatory=$true)] $Password,
+    [string] [Parameter(Mandatory=$true)] $TenantId
+)
+
+$segment = $TestFilePath.Split("\")
+$testFile = $segment[$segment.Length - 1].Replace(".ps1", "");
+$outputFile = "$OutputDirectory\TEST-$testFile-$BuildNumber.xml"
+
+$parameters = @{ ResourceGroupName = $ResourceGroupName; SrcDirectory = $SrcDirectory; Username = $Username; Password = $Password; TenantId = $TenantId }
+$script = @{ Path = $TestFilePath; Parameters = $parameters }
+
+Invoke-Pester -Script $script -OutputFile $outputFile -OutputFormat NUnitXml
